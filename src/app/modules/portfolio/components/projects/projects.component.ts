9205a4243a935aad4bdf3708903a01e6490ac51f
joao-silva09 <joao.e.silva.03@gmail.com>
2024-09-05 13:49:42 -0300

    7 - Projects Component.

---
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -0,0 +1,29 @@
+import { Component, signal } from '@angular/core';
+import { IProjects } from '../../interface/IProjects.interface';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss',
+})
+export class ProjectsComponent {
+  public arrayProjects = signal<IProjects[]>([
+    {
+      src: 'TODO',
+      alt: 'Projeto Vida FulStack',
+      title: 'Vida FullStack',
+      width: '100px',
+      height: '51px',
+      description:
+        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'TODO',
+        },
+      ],
+    },
+  ]);
+}
